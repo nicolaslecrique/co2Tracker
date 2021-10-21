236d9a4f12322eac24d8d2d37b88305717322a0d
Nicolas <nicolas.lecrique+git@globers.co>
2021-10-21 07:16:06 +0200

    add meal widget file (title + 3 buttons + portion if needed)

---
+++ lib/routes/daily_emissions_form/widgets/meal.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Meal extends StatefulWidget {
+  const Meal({Key? key}) : super(key: key);
+
+  @override
+  _MealState createState() => _MealState();
+}
+
+class _MealState extends State<Meal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
